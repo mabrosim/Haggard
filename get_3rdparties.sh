daccb8b31e170cf83706f9a07a6bbbaf5e883d83
Maxim Abrosimov <mabrosim@gmail.com>
2014-08-08 16:58:42 +0300

    WIP: get 3rdparties script

---
+++ get_3rdparties.sh
@@ -0,0 +1,58 @@
+
+echo "Fetching 3rdparties..."
+
+# HINT
+# git archive --remote=git://git.foo.com/project.git HEAD:path/to/directory filename
+# http://stackoverflow.com/questions/1125476/git-retrieve-a-single-file-from-a-repository
+
+cd 3rdparty
+
+#phplot
+#http://sourceforge.net/projects/phplot/files/latest/download
+
+#jqplot
+#https://bitbucket.org/cleonello/jqplot/wiki/Home
+
+#WGET
+
+#ezSQL
+git clone https://github.com/ezSQL/ezSQL.git
+
+#php-profiler
+git clone https://github.com/jimrubenstein/php-profiler.git
+
+#js-min
+git clone https://github.com/rgrove/jsmin-php.git
+#http://www.crockford.com/javascript/jsmin.zip
+
+#js-packer, not needed ?
+
+#jquery
+
+#jquery-cookie
+git clone https://github.com/carhartl/jquery-cookie.git
+
+#jquery-dragsort
+#http://dragsort.codeplex.com/downloads/get/887234
+
+#jquery-farbtastic
+git clone https://github.com/mattfarina/farbtastic.git
+
+#jquery-livequery , http://plugins.jquery.com/livequery/
+git clone https://github.com/hazzik/livequery.git
+
+#jquery-placeholder
+git clone https://github.com/matoilic/jquery.placeholder.git
+
+#jquery-qtip
+#http://craigsworks.com/projects/qtip/download/#
+git clone https://github.com/Craga89/qTip1.git
+
+#jquery-tablesorter
+git clone https://github.com/Mottie/tablesorter.git
+
+#jquery-ui
+git clone https://github.com/jquery/jqueryui.com.git
+
+#PHPExcel
+#https://phpexcel.codeplex.com/SourceControl/latest#
